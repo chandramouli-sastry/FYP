bdf43d87db2ad8a84d89f443aedc28f4e74def58
Debarati <debarati.d1994@gmail.com>
2017-02-12 13:41:42 +0530

    obtain_listcorrelation.py changed

--- obtain_listcorrelation.py
+++ obtain_listcorrelation.py
@@ -1,52 +1,69 @@
 from scipy.stats.stats import pearsonr   
 from random import randint
+import random
 
 def calc_correlation (list1,list2):
 	print pearsonr(list1,list2)
 
-def getRandomList(minR,maxR):
+
+def getNewList(minR,maxR,passedlist):
 	templist=[]
-	for i in range(0,maxR):
-		templist.append(randint(minR,maxR))
-	print "Temporary list generated : "+str(templist)
+	random_list=random.sample(range(1, 100), len(passedlist))
+	for i in range(0,len(passedlist)):
+		if passedlist[i] in mapping:
+			templist.append(mapping[passedlist[i]])
+		else:
+			mapping[passedlist[i]]=random_list[i]
+			templist.append(mapping[passedlist[i]])
+
+	print "temporary list created is :"+str(templist)
+	print mapping
 	return templist
-
-def check_typeof_list(list1,list2):
-	#assume that if the first element is a string, the rest are strings too 
-	#This is inline with the clarification that mixed lists are not possible
 	
-	list1Flag=False
-	list2Flag=False
+def calculate_list(list1,list2,list1Flag,list2Flag):
 	lenlist1=len(list1)
-	if isinstance(list1[0], (str, unicode)):
-		list1Flag=True
-	if isinstance(list2[0], (str, unicode)):
-		list2Flag=True
-
 	if list1Flag and list2Flag:
 		print "Both are string lists"
-		list1=getRandomList(1,lenlist1)
-		list2=getRandomList(1,lenlist1)
+		list1=getNewList(1,lenlist1,list1)
+		list2=getNewList(1,lenlist1,list2)
 		calc_correlation(list1,list2)
 	elif list1Flag:
+		list2=map(int, list2)
 		print "1 is a string list"
-		list1=getRandomList(1,lenlist1)
+		list1=getNewList(1,lenlist1,list1)
 		calc_correlation(list1,list2)
 	elif list2Flag:
+		list1=map(int, list1)
 		print "2 is a string list"
-		list2=getRandomList(1,lenlist1)
+		list2=getNewList(1,lenlist1,list2)
 		calc_correlation(list1,list2)
 	else:
+		list1=map(int, list1)
+		list2=map(int, list2)
 		print "both are number list"
 		calc_correlation(list1,list2)
 
-
+mapping=dict()
 def main():
 	list1 = raw_input().split() 
 	list2 = raw_input().split()
+
+	try:
+		int(list1[0])
+	except ValueError:
+		list1Flag = True
+	else:
+		list1Flag= False # not a string
+	try:
+		int(list2[0])
+	except ValueError:
+		list2Flag = True
+	else:
+		list2Flag= False
+ 
 	#Assumption : both lists are same length ; else correlation cant be calculated
 	if len(list1)!=len(list2):
 		print "length of both lists need to match! Try again"
 	else:
-		check_typeof_list(list1,list2)
+		calculate_list(list1,list2,list1Flag,list2Flag)
 main()
