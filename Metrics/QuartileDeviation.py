698efb4cc179afe1d4e9c4d175bd216f2aa87f2f
darshan-sj <calltodarshan@gmail.com>
2017-02-21 09:49:20 +0530

    Metric-Inter Quartile Deviation Implemented

---
+++ Metrics/QuartileDeviation.py
@@ -0,0 +1,17 @@
+from __future__ import division
+def compute_Q1_Q2(array):
+    temp = sorted(array)
+    return temp[int((0.25)*len(temp))],temp[int((0.75*len(temp)))]
+
+def compute(array):
+    Q1_value,Q2_value = compute_Q1_Q2(array)
+    result = list()
+    for i in array:
+        if i<=Q1_value:
+            result.append(Q1_value-i)
+        elif i>=Q2_value:
+            result.append(i-Q2_value)
+        else:
+            result.append(0)
+
+    return result
