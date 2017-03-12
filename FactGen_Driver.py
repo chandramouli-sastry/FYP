9793b5554e92ed04ca2215bdd287ff520987386c
darshan-sj <calltodarshan@gmail.com>
2017-03-12 22:20:02 +0530

    modified FactGen_Driver to include simple statistic fact

--- FactGen_Driver.py
+++ FactGen_Driver.py
@@ -10,3 +10,9 @@ num_fact = NumericRatioFact()
 num_fact.fuzzy_intersection()
 # with open("Resources/fact_pickle.pkl","w") as f:
 #     pickle.dump(num_fact,f)
+
+##############################################################
+from FactGen.NumericSimpleFact import NumericSimpleFact
+
+num_fact_simple = NumericSimpleFact()
+num_fact.fuzzy_intersection()
