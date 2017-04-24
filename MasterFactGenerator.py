b4d4250fcaac07562a0886ad0c5de490171487df
darshan-sj <calltodarshan@gmail.com>
2017-04-24 21:19:24 +0530

    Saving masterfactgen

--- MasterFactGenerator.py
+++ MasterFactGenerator.py
@@ -13,7 +13,9 @@ def gen_simple():
     fields = discrete_fields + new_fields
     count = 0
     thresh = 1
-    for field in fields:
+    if "Dist_Other" in fields:
+        fields.remove("Dist_Other")
+    for field in fields[:len(fields)//2]:
         path = "JSONS/Simple/Fact_{}.json".format(field)
         print(field)
         if not (os.path.isfile(path)):
@@ -72,6 +74,6 @@ def gen_ratio():
             print("{}% completed".format(perc))
             thresh += 1
 
-#gen_simple()
-gen_semantic()
+gen_simple()
+#gen_semantic()
 #gen_ratio()
